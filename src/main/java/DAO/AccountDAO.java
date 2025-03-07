10fd8b973955aa3ccd54717f39ddef2b26318925
Hien Pham <hpham474@gmail.com>
2025-03-07 00:30:47 +0000

    create accountDAO

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,5 +1,34 @@
 package DAO;
 
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AccountDAO {
-    
+    public Account getAccountByUser (String user) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE username = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, user);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()) {
+                Account account = new Account(
+                    rs.getInt("account_id"),
+                    rs.getString("username"),
+                    rs.getString("password")
+                );
+                return account;
+            }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
