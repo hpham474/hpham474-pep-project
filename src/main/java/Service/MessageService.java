5615d36dcef1ccc131239e870f0cada8885c5a90
Hien Pham <hpham474@gmail.com>
2025-03-07 02:11:03 +0000

    set up messageService

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,19 @@
 package Service;
 
+import Model.Message;
+import DAO.MessageDAO;
+
+
 public class MessageService {
+    public MessageDAO messageDAO;
+
+    public MessageService() {
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+    }
+
     
 }
