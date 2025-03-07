f900ee5bfac0bfb01164345b366bae2205fe5825
Hien Pham <hpham474@gmail.com>
2025-03-07 02:06:14 +0000

    complete accountService

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,35 @@
 package Service;
 
+import Model.Account;
+import DAO.AccountDAO;
+
 public class AccountService {
-    
+    private AccountDAO accountDAO;
+
+    public AccountService() {
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    public Account registerUser(Account account) {
+        if (account.getUsername() != "" &&
+            account.getPassword().length() >= 4 && 
+            accountDAO.getAccountByUser(account.getUsername()) == null) {
+                return accountDAO.insertAccount(account);
+            }
+
+        return null;
+    }
+
+    public Account loginUser(Account account) {
+        if (accountDAO.getAccountByUser(account.getUsername()).getPassword() == account.getPassword()) {
+            return account;
+        }
+
+        return null;
+    }
+
 }
