15f87ae919f3e39e1fe1a54cdaf121751ac5398a
Hien Pham <hpham474@gmail.com>
2025-03-07 07:43:22 +0000

    add methods to MessageService

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -3,6 +3,7 @@ package Service;
 import Model.Message;
 import DAO.MessageDAO;
 
+import java.util.List;
 
 public class MessageService {
     public MessageDAO messageDAO;
@@ -15,5 +16,32 @@ public class MessageService {
         this.messageDAO = messageDAO;
     }
 
-    
+    public List<Message> getAllMessages() {
+        return messageDAO.getAllMessages();
+    }
+
+    public List<Message> getMessageByUser(int userID) {
+        return messageDAO.getMessagesByUser(userID);
+    }
+
+    public Message getMessageByID(int id) {
+        return messageDAO.getMessageById(id);
+    }
+
+    public Message insertMessage(Message message) {
+        if (message.getMessage_text().length() <= 255 &&
+            message.getMessage_text() != "") {
+                return messageDAO.insertMessage(message);
+            }
+
+        return null;
+    }
+
+    public Message deleteMessage(int id, Message message) {
+        return messageDAO.deleteMessage(id, message);
+    }
+
+    public Message updateMessage(int id, Message message) {
+        return messageDAO.updateMessage(id, message);
+    }
 }
